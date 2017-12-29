f61e8a8886564f87b0ec2ef615b9d60f0ace8b6e
encadyma <encadyma@gmail.com>
2017-12-28 17:00:05 -0800

    Linked editor pages to router

--- src/router.js
+++ src/router.js
@@ -3,14 +3,16 @@ import VueRouter from 'vue-router';
 import HomePage from './HomePage.vue';
 import EditorFrame from './EditorFrame.vue';
 import EditorUpload from './Editor/EditorUpload.vue';
+import EditorEdit from './Editor/EditorEdit.vue';
+import EditorPublish from './Editor/EditorPublish.vue';
 
 const routes = [
   {
     path: '/editor',
     component: EditorFrame,
     children: [
-      { path: 'publish', component: EditorUpload },
-      { path: 'edit', component: EditorUpload },
+      { path: 'publish', component: EditorPublish },
+      { path: 'edit', component: EditorEdit },
       { path: 'upload', component: EditorUpload },
       { path: '**', redirect: 'upload' },
     ]
